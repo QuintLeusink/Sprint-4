24b789e3b2355515010497e938770a5ffae1da46
Quint Leusink <0308410@student.rocvantwente.nl>
2019-04-16 12:36:13 +0200

    alles

---
+++ scrum/sprint 3/S1-BE-M08-Fouten/8.5 sprint 3/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            for (int i = 1; i < 100; i++)
+            {
+                int getal1 = rnd.Next(0, int.MaxValue);
+                int getal2 = rnd.Next(0, int.MaxValue);
+                int product = Math.Abs(getal1 * getal2);
+                Teller(getal1, getal2, product);
+            }
+            Console.ReadKey();
+        }
+        private static void Teller(int getal1, int getal2, int product)
+        {
+            Console.WriteLine("{0} * {1} = {2}", getal1, getal2, product);
+        }
+    }
+}
---
+++ scrum/sprint 4/SE-BE-M09/9.1/ConsoleApp9.1 sprint 4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Opdracht9._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bestandNaam, tekst;
+            Console.WriteLine("Geef een naam voor het tekstbestand:");
+            bestandNaam = Console.ReadLine();
+
+            try
+            {
+                if (File.Exists(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop) + "\\" + bestandNaam + ".txt"))
+                {
+                    DialogResult dialogResult = MessageBox.Show(bestandNaam + ".txt bestaat al," + "\n" + "Wilt u het bestand vervangen ?", "Opslaan", MessageBoxButtons.YesNo);
+                    switch (dialogResult)
+                    {
+                        case DialogResult.Yes:
+                            goto START;
+                        case DialogResult.No:
+                            Environment.Exit(0);
+                            break;
+                    }
+                }
+                else
+                {
+                    goto START;
+                }
+            START:
+                {
+                    Console.WriteLine("Geef uw tekst:");
+                    tekst = Console.ReadLine();
+                    StreamWriter sw = new StreamWriter(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop) + "\\" + bestandNaam + ".txt");
+                    sw.WriteLine(tekst);
+                    sw.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Mislukt: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Opgeslagen!");
+            }
+            System.Diagnostics.Process.Start(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop) + "\\" + bestandNaam + ".txt");
+            Console.ReadLine();
+        }
+    }
+}
